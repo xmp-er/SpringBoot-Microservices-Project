7f18e2a2da9989431dffeb4374bfd3febe46038a
xmp-er <107166230+xmp-er@users.noreply.github.com>
2023-09-13 01:50:39 +0530

    Called doPayment API with FeignClient from placeOrder

--- OrderService/src/main/java/com/dailycodebuffer/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/com/dailycodebuffer/OrderService/service/OrderServiceImpl.java
@@ -1,7 +1,9 @@
 package com.dailycodebuffer.OrderService.service;
 
 import com.dailycodebuffer.OrderService.entity.Order;
+import com.dailycodebuffer.OrderService.external.client.PaymentService;
 import com.dailycodebuffer.OrderService.external.client.ProductService;
+import com.dailycodebuffer.OrderService.external.request.PaymentRequest;
 import com.dailycodebuffer.OrderService.model.OrderRequest;
 import com.dailycodebuffer.OrderService.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
@@ -19,6 +21,9 @@ public class OrderServiceImpl implements OrderService{
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private PaymentService paymentService;
     @Override
     public Long placeOrder(OrderRequest orderReq) {
 
@@ -35,6 +40,28 @@ public class OrderServiceImpl implements OrderService{
         od.setProductId(orderReq.getProductId());
 
         od = orderRepo.save(od);
+
+        log.info("Calling Payment Service to complete the payment");
+
+        PaymentRequest paymentRequest = new PaymentRequest();
+        paymentRequest.setPaymentMode(orderReq.getPaymentMode());
+        paymentRequest.setOrderId(orderReq.getProductId());
+        paymentRequest.setAmount(orderReq.getTotalAmount());
+
+        String orderStatus = null;
+        try{
+            paymentService.doPayment(paymentRequest);
+            log.info("Payment done successfully,changing the order status");
+            orderStatus = "PLACED";
+        }catch(Exception e){
+            log.error("Error occurred in payment,changing order status to PAYMENT_FAILED");
+            orderStatus = "PAYMENT_FAILED";
+        }
+
+        od.setOrderStatus(orderStatus);
+        orderRepo.save(od);
+
+
         log.info("Order successfully place with the details {}",od);
         return od.getId();
     }
--- PaymentService/src/main/java/com/example/PaymentService/service/PaymentServiceImpl.java
+++ PaymentService/src/main/java/com/example/PaymentService/service/PaymentServiceImpl.java
@@ -4,12 +4,14 @@ import com.example.PaymentService.entity.TransactionDetails;
 import com.example.PaymentService.model.PaymentRequest;
 import com.example.PaymentService.repository.TransactionDetailsRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
 public class PaymentServiceImpl implements PaymentService{
 
+    @Autowired
     private TransactionDetailsRepository transactionDetailsRepository;
 
     @Override
